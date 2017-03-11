f1ac2da2db90c4492319f776181cfc3e30d49f1d
Warren Chan <warrenchan@Warrens-MacBook-Pro.local>
2017-03-10 19:39:17 -0800

    restyled css

--- js/main.js
+++ js/main.js
@@ -8,12 +8,9 @@ $(document).ready(function () {
 	
 
 	$('#selector').on('change', function () {
-
 		$('.container').addClass('compactHeader');
 		$('#news').empty();
-
 		loadingMessage.show();
-
 		userSelect = this.value;
 
 		// Built by LucyBot. www.lucybot.com
@@ -22,54 +19,36 @@ $(document).ready(function () {
 			'api-key': "049673d3a2eb4eda9c6bb4ec46c49cf5",
 			'callback': "12"
 		});
+
 		$.ajax({
 			url: url,
 			method: 'GET',
 		})
 
-			.done(function (result) {
-				console.log(result);
-
-
-				function checkPictures(artical) {
-					console.log(artical.multimedia.length)
-					return artical.multimedia.length;
-				}
-
-				var filtered = result.results.filter(checkPictures)
-				filtered.splice(12);
-				console.log(filtered)
-
-
-				var newsString = '';
-
-				$.each(filtered, function (item, value) {
-
-					//variances & markups
-
-					var link = value.url;
-					var abstract = value.abstract;
-					var image = value.multimedia[4].url;
-
-					newsString += '<li><a href=' + link + ' target="_blank">';
-					newsString += '<div class="storyImage" style="background-image: url(' + image + ')">';
-					newsString += '<div class="abstractText">' + '<p>' + abstract + '</p>' + '</div>';
-					newsString += '</div></a></li>';
-					console.log(newsString);
-
-				});
-
-				loadingMessage.hide();
-
-				$('#news').append(newsString);
-
-			})
-
-			.fail(function () {
-
-				loadingMessage.hide();
-
-				$('#news').append('<div class="err">' + '<p>Got nothing for you...</p>' + '</div>')
+		.done(function (result) {
+			function checkPictures(artical) {
+				return artical.multimedia.length;
+			}
+			var filtered = result.results.filter(checkPictures)
+			filtered.splice(12);
+			var newsString = '';
+			$.each(filtered, function (item, value) {
+				//variances & markups
+				var link = value.url;
+				var abstract = value.abstract;
+				var image = value.multimedia[4].url;
+				newsString += '<li><a href=' + link + ' target="_blank">';
+				newsString += '<div class="storyImage" style="background-image: url(' + image + ')">';
+				newsString += '<div class="abstractText">' + '<p>' + abstract + '</p>' + '</div>';
+				newsString += '</div></a></li>';
 			});
+			loadingMessage.hide();
+			$('#news').append(newsString);
+		})
+
+		.fail(function () {
+			loadingMessage.hide();
+			$('#news').append('<div class="err">' + '<p>Got nothing for you...</p>' + '</div>')
+		});
 	});
 });
